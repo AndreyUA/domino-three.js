e8b820e21f78cc984ff66a9a7df897b001c3442e
AndreyUA <infernalsteel666@gmail.com>
2025-03-06 17:59:46 +0200

    added responsive view

--- src/main.ts
+++ src/main.ts
@@ -67,3 +67,17 @@ const tick = () => {
   window.requestAnimationFrame(tick);
 };
 tick();
+
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
