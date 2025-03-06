2eb1363441789ed4f143f1662d289f63fea382e3
AndreyUA <infernalsteel666@gmail.com>
2025-03-06 16:49:54 +0200

    installed rapier

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,7 @@ import "./style.css";
 
 import * as THREE from "three";
 
+import RAPIER from "@dimforge/rapier3d-compat";
+
 console.log("Hello, three.js ! ", THREE);
+console.log("Hello, rapier ! ", RAPIER);
